6e9cc6877e34e57ebd7a7e13e625fc16a41890bb
Mason Guaman <masonstalin@gmail.com>
2022-10-27 14:10:57 -0400

    ty

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var TextField: UITextField!
+    
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = TextField.text
     }
 
 
